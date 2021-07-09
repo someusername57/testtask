8c0adeb6517b57f62601754e4e5918d4ec4b954e
My Name <my@email.com>
2021-07-09 08:35:20 +0300

    refractored Application.php, Utilities.php changed layout

--- src/classes/KanbanBoard/Application.php
+++ src/classes/KanbanBoard/Application.php
@@ -10,106 +10,125 @@ use \Michelf\Markdown;
 
 class Application {
 
-	public function __construct($github, $repositories, $paused_labels = array())
-	{
-		$this->github = $github;
-		$this->repositories = $repositories;
-		$this->paused_labels = $paused_labels;
-	}
+    public function __construct($github, $repositories, $paused_labels = array())
+    {
+        $this->github = $github;
+        $this->repositories = $repositories;
+        $this->paused_labels = $paused_labels;
+    }
 
-	public function board()
-	{
-		$ms = array();
-		foreach ($this->repositories as $repository)
-		{
-			foreach ($this->github->milestones($repository) as $data)
-			{
-				$ms[$data['title']] = $data;
-				$ms[$data['title']]['repository'] = $repository;
-			}
-		}
-		ksort($ms);
-		foreach ($ms as $name => $data)
-		{
-			$issues = $this->issues($data['repository'], $data['number']);
-			$percent = self::_percent($data['closed_issues'], $data['open_issues']);
-			if($percent)
-			{
-				$milestones[] = array(
-					'milestone' => $name,
-					'url' => $data['html_url'],
-					'progress' => $percent,
-					'queued' => $issues['queued'],
-					'active' => $issues['active'],
-					'completed' => $issues['completed']
-				);
-			}
-		}
-		return $milestones;
-	}
+    public function board()
+    {
+        $milestones_github = array();
+        $milestones = array();
+        foreach ($this->repositories as $repository) {
+            foreach ($this->github->milestones($repository) as $data) {
+                $data['repository'] = $repository;
+                $milestones_github[$data['title']] = $data;
+            }
+        }
+        ksort($milestones_github);
+        foreach ($milestones_github as $name => $data) {
+            $issues = $this->issues($data['repository'], $data['number']);
+            $percent = self::percent($data['closed_issues'], $data['open_issues']);
+            if ($percent) {
+                $milestones[] = array(
+                    'milestone' => $name,
+                    'url'       => $data['html_url'],
+                    'progress'  => $percent,
+                    'queued'    => $issues['queued'],
+                    'active'    => $issues['active'],
+                    'completed' => $issues['completed']
+                );
+            }
+        }
+        return $milestones;
+    }
 
-	private function issues($repository, $milestone_id)
-	{
-		$i = $this->github->issues($repository, $milestone_id);
-		foreach ($i as $ii)
-		{
-			if (isset($ii['pull_request']))
-				continue;
-			$issues[$ii['state'] === 'closed' ? 'completed' : (($ii['assignee']) ? 'active' : 'queued')][] = array(
-				'id' => $ii['id'], 'number' => $ii['number'],
-				'title'            	=> $ii['title'],
-				'body'             	=> Markdown::defaultTransform($ii['body']),
-     'url' => $ii['html_url'],
-				'assignee'         	=> (is_array($ii) && array_key_exists('assignee', $ii) && !empty($ii['assignee'])) ? $ii['assignee']['avatar_url'].'?s=16' : NULL,
-				'paused'			=> self::labels_match($ii, $this->paused_labels),
-				'progress'			=> self::_percent(
-											substr_count(strtolower($ii['body']), '[x]'),
-											substr_count(strtolower($ii['body']), '[ ]')),
-				'closed'			=> $ii['closed_at']
-			);
-		}
-		usort($issues['active'], function ($a, $b) {
-			return count($a['paused']) - count($b['paused']) === 0 ? strcmp($a['title'], $b['title']) : count($a['paused']) - count($b['paused']);
-		});
-		return $issues;
-	}
+    private function issues($repository, $milestone_id)
+    {
+        $issues = array(
+            'active'    => array(),
+            'queued'    => array(),
+            'completed' => array()
+        );
+        $issues_github = $this->github->issues($repository, $milestone_id);
+        
+        foreach ($issues_github as $issue_item) {
+            if (isset($issue_item['pull_request'])) {
+                continue;
+            }
+            
+            $issues[self::state($issue_item)][] = array(
+                'id'        => $issue_item['id'],
+                'number'    => $issue_item['number'],
+                'title'     => $issue_item['title'],
+                'body'      => Markdown::defaultTransform($issue_item['body']),
+                'url'       => $issue_item['html_url'],
+                'assignee'  => self::assignee($issue_item),
+                'paused'    => self::labels_match($issue_item, $this->paused_labels),
+                'progress'  => self::percent(
+                                    substr_count(strtolower($issue_item['body']), '[x]'),
+                                    substr_count(strtolower($issue_item['body']), '[ ]')
+                                ),
+                'closed'    => $issue_item['closed_at']
+            );
+        }
+        
+        usort($issues['active'], function ($a, $b) {
+            return (count($a['paused']) - count($b['paused']) === 0) ? strcmp($a['title'], $b['title']) : count($a['paused']) - count($b['paused']);
+        });
+        return $issues;
+    }
 
-	private static function _state($issue)
-	{
-		if ($issue['state'] === 'closed')
-			return 'completed';
-		else if (Utilities::hasValue($issue, 'assignee') && count($issue['assignee']) > 0)
-			return 'active';
-		else
-			return 'queued';
-	}
+    private static function state($issue)
+    {
+        if ($issue['state'] === 'closed') {
+            return 'completed';
+        } elseif (Utilities::hasValue($issue, 'assignee') && count($issue['assignee']) > 0){
+            return 'active';
+        } else {
+            return 'queued';
+        }
+    }
+    
+    private static function assignee($issue)
+    {
+        if (is_array($issue)
+            && array_key_exists('assignee', $issue)
+            && !empty($issue['assignee'])
+        ) {
+            return $issue['assignee']['avatar_url'].'?s=16';
+        } else {
+            return null;
+        }
+    }
+    
+    private static function labels_match($issue, $needles)
+    {
+        if (Utilities::hasValue($issue, 'labels')) {
+            foreach ($issue['labels'] as $label) {
+                if (in_array($label['name'], $needles)) {
+                    return array($label['name']);
+                }
+            }
+        }
+        return array();
+    }
 
-	private static function labels_match($issue, $needles)
-	{
-		if(Utilities::hasValue($issue, 'labels')) {
-			foreach ($issue['labels'] as $label) {
-				if (in_array($label['name'], $needles)) {
-					return array($label['name']);
-				}
-			}
-		}
-		return array();
-
-	}
-
-	private static function _percent($complete, $remaining)
-	{
-		$total = $complete + $remaining;
-		if($total > 0)
-		{
-			$percent = ($complete OR $remaining) ? round($complete / $total * 100) : 0;
-			return array(
-				'total' => $total,
-				'complete' => $complete,
-				'remaining' => $remaining,
-				'percent' => $percent
-			);
-		}
-		return array();
-	}
+    private static function percent($complete, $remaining)
+    {
+        $total = $complete + $remaining;
+        if($total > 0)
+        {
+            $percent = ($complete OR $remaining) ? round($complete / $total * 100) : 0;
+            return array(
+                'total' => $total,
+                'complete' => $complete,
+                'remaining' => $remaining,
+                'percent' => $percent
+            );
+        }
+        return array();
+    }
 }
--- src/classes/Utilities.php
+++ src/classes/Utilities.php
@@ -3,7 +3,8 @@ namespace KanbanBoard;
 
 class Utilities
 {
-    public static function init(){
+    public static function init()
+    {
         $config = fopen(__DIR__."/../config", "r");
         if ($config) {
             while (($envVar = fgets($config, 4096)) !== false) {
@@ -12,7 +13,8 @@ class Utilities
         }
     }
     
-    public static function env($name, $default = null) {
+    public static function env($name, $default = null)
+    {
         $value = getenv($name);
         if ($default !== null) {
             if (!empty($value)) {
@@ -23,11 +25,13 @@ class Utilities
         return (empty($value) && $default === null) ? die('Environment variable ' . $name . ' not found or has no value') : $value;
     }
 
-    public static function hasValue($array, $key) {
+    public static function hasValue($array, $key)
+    {
         return is_array($array) && array_key_exists($key, $array) && !empty($array[$key]);
     }
 
-    public static function dump($data) {
+    public static function dump($data)
+    {
         echo '<pre>';
         var_dump($data);
         echo '</pre>';
